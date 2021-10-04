4aacfccabb6b55ae01655792ff0c724651d49495
Kyle Kovalchick <kovalchickzz@gmail.com>
2021-10-03 23:41:14 -0500

    Created simple app to start playing around

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
+using System.Windows.Input;
+using ReactiveUI;
 
 namespace OpenMissile.ViewModels {
     public class MainWindowViewModel : ViewModelBase {
-        public string Greeting => "Welcome to Avalonia!";
+        public MainWindowViewModel() {
+            LaunchRocketCommand = ReactiveCommand.Create(() => {
+                Trace.WriteLine("Launched Rocket");
+            });
+        }
+
+        public ICommand LaunchRocketCommand { get; }
     }
+    
 }
